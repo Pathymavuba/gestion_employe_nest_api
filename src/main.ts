be2ee1fbb77905fe78d35ebdc30c8632f7dfcfe0
pathymavuba <mavubapathy@gmail.com>
2023-06-09 15:49:19 +0100

    master : add envirronnement variables

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(
+    process.env.PORT ? parseInt(process.env.PORT) : 3000,
+    function () {
+      console.log(`server is running on ${process.env.PORT}`);
+    },
+  );
 }
 bootstrap();
